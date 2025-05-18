61cc48644ad740eba274c600d39143c77ad970d6
Md. Shafiul Alam <shafiulahcb1007@gmail.com>
2025-05-18 23:51:43 +0600

    First commit

---
+++ Assignment_1/Controllers/SignupController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: SignupController
+        public ActionResult SignupPage()
+        {
+            return View();
+        }
+    }
+}
